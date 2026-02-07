ec894b1e810692fc5cfdfec7b01fdc4d428905cb
Oghenemaro Kimberly Emuophedaro <oke201@exeter.ac.uk>
2026-02-07 19:34:55 +0000

    Delete client/configs directory

--- client/configs/stages.js
+++
@@ -1,7 +0,0 @@
-/**
- * Pseudocode:
- * 1) Re-export shared stage configuration.
- * 2) Replace relative import with aliases later.
- */
-
-export { CONFIG_DEFINITIONS, STAGE_CONFIGS, getStageConfigById } from "../../shared/stages";
